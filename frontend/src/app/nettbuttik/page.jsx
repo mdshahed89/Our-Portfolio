aeb4295e915e1bd29a19dc49d897e76469a75225
Farsit-007 <public>
2024-12-15 11:21:13 +0600

    Done

--- frontend/src/app/nettbuttik/page.jsx
+++ frontend/src/app/nettbuttik/page.jsx
@@ -30,7 +30,7 @@ const page = () => {
                 <p>
                   Vi vil deretter ta kontakt med deg for å tilby hjelp og
                   veiledning i å finne den optimale løsningen som passer best
-                  for deg – og det er helt uforpliktende!s
+                  for deg - og det er helt uforpliktende!s
                 </p>
               </div>
             </div>
