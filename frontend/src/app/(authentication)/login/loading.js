3d1bf5f56f4706e6c57faffa2ddabaf065bde13a
mdshahed89 <mdshahedprofile@gmail.com>
2024-12-22 03:01:15 +0600

    done 3

---
+++ frontend/src/app/(authentication)/login/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className=" h-[100vh] w-[100vw] bg-white flex items-center justify-center ">
+      <div className="w-10 h-10 animate-[spin_1s_linear_infinite] rounded-full border-double border-4 border-r-0 border-l-0 border-b-green-400 border-t-green-700"></div>
+    </div>
+  );
+};
+
+export default loading;
