43b33805e03a789d01ea36bd12a312d89b14cb3c
mdshahed89 <mdshahedprofile@gmail.com>
2025-01-11 00:34:05 +0600

    manually add origin on backend

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 dotenv.config();
 dbConnect();
 const corsOptions = {
-  origin: [process.env.FRONTEND_URL],
+  origin: ['https://sidesone.no', 'http://localhost:3001'],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
